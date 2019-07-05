dd99af9f94d88d2525fb0e3b1189880f7a0a1862
ZetaGH <jose.veliz.francia@gmail.com>
2019-07-04 21:44:25 -0500

    Initial Commit

---
+++ focused/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  focused
+//
+//  Created by Jose Veliz on 7/4/19.
+//  Copyright © 2019 Jose Veliz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
